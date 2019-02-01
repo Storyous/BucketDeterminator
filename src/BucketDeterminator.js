9dca138631e7f26e7fba714247a4edd22e977d93
Ivo Šofránek <sofranek@outlook.com>
2019-02-01 11:15:06 +0100

    Merged object id validations to one

--- src/BucketDeterminator.js
+++ src/BucketDeterminator.js
@@ -1,12 +1,8 @@
 
 module.exports = (bucketSize = 32) => {
     const determine = (objectId) => {
-        if (objectId.length !== 24) {
-            throw new Error('ObjectId must have length of 24 characters');
-        }
-
-        if (!/[0-9A-Fa-f]{24}/g.test(objectId)) {
-            throw new Error('ObjectId must consist of hex compatible characters');
+        if (!/^[0-9a-f]{24}$/g.test(objectId)) {
+            throw new Error('ObjectId must consist of hex compatible characters and have length of 24 characters');
         }
 
         const random = objectId.substr(-6, 6);
